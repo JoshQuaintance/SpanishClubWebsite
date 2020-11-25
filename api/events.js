2d45daa3185134539d85bbf56e43d48e8be786ee
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-11-25 17:47:27 -0500

    Feed Events | Display Events | Auto Delete Events

--- api/events.js
+++ api/events.js
@@ -1,7 +1,3 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -9,41 +5,67 @@ mongoose
 	.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.catch(err => console.error(err));
 
-const eventSchema = new mongoose.Schema({
-	name        : { type: String, required: true },
-	description : { type: String, require: true },
-	date        : { type: String, require: true },
-	time        : { type: String, require: true },
-	location    : { type: String, require: true }
-});
-
 module.exports = function(app) {
+	const eventSchema = new mongoose.Schema(
+		{
+			title     : { type: String, required: true },
+			date      : { type: String, require: true },
+			time      : { type: String, require: true },
+			location  : { type: String, require: true },
+			expire_at : { type: Date, default: Date.now, expires: 60 }
+		},
+		{ timestamps: true }
+	);
+
 	// Create a new event into the database
 	app.post('/events', async (req, res) => {
+		let exactTime;
+		if (req.body.time.indexOf('PM') && req.body.time.split(' ')[0].substr(0, 2) != 12) {
+			let fullTime = req.body.time.split(' ')[0].length < 5 ? `0${req.body.time}` : req.body.time;
+			let hour =
+				fullTime.split(' ')[0].substr(0, 2) * 1 + 12 < 10
+					? `0${fullTime.split(' ')[0].substr(0, 2) * 1 + 12}`
+					: fullTime.split(' ')[0].substr(0, 2) * 1 + 12;
+
+			exactTime = `${hour}:${fullTime.split(' ')[0].substr(3, 2)}`;
+		}
+		let eventFinished = new Date(`${req.body.date}, ${req.body.year} ${exactTime}`);
+
 		// Declare event model
 		const Event = mongoose.model('Events', eventSchema);
 
 		const newEvent = new Event({
-			name        : req.body.name,
-			description : req.body.description,
-			date        : req.body.date,
-			time        : req.body.time,
-			location    : req.body.location
+			/*
+            {
+                title    : "title",
+                date     : "Month Date",
+                time     : "12:00 PM"
+                location : "Location"
+            }
+            */
+			title     : req.body.title,
+			date      : req.body.date,
+			time      : req.body.time,
+			location  : req.body.location,
+			expire_at : eventFinished
 		});
 
 		newEvent.save(err => {
-			if (err) console.error(err);
+			if (err) {
+				res.status(500).send(err);
+				console.error(err);
+			} else {
+				res.status(201).send('Event Created');
+			}
 		});
-
-		res.status(201).send('Event Created');
 	});
 
 	// Fetch Events from Database
 	app.get('/events', async (req, res) => {
 		// Declare event model
-		const Event = mongoose.model('Events', eventSchema);
+		const Events = mongoose.model('Events', eventSchema);
 
-		await Event.find({}, (err, data) => {
+		await Events.find({}, (err, data) => {
 			if (err) {
 				res.status(500).send('Cannot Fetch Events');
 				console.error(err);
--- public/js/events.js
+++ public/js/events.js
@@ -9,67 +9,130 @@
   }
 */
 
-let upComingEvents = [
-	{
-		title       : 'event1',
-		description : 'event one desc',
-		date        : '2020/11/03',
-		time        : '20:00',
-		location    : "Tobie's room"
-	},
-	{
-		title       : 'event1',
-		description : 'We are doing ',
-		date        : '2020/11/03',
-		time        : '20:00',
-		location    : "Tobie's room"
-	}
-];
+/*
+<div class="1 event-content justify-content-start text-left">
+        <hr class="my-4">
+            
+            <div class="wrapper d-flex flex-row pl-3 pr-3">
+                <div class="wrapper date mr-5 text-center d-flex flex-column align-items-center justify-content-center">
+                    <h1 class="date-number" id="testing-dates">
+                        12
+                    </h1>
+                    <h4 class="month">
+                        March
+                    </h4>
+                </div>
+                    
+                <div class="wrapper info-bar time-location d-flex justify-content-between flex-column">
+                    <div class="wrapper d-flex align-items-start mt-3" id="information-bar">
+                        <ul>
+                            <li>
+                                <i class="im im-info"></i>
+                            </li>
+                            <li><span><strong></strong><span class="title">Sidney Spanish Club</span></span></li>
+                        </ul>
+                    </div>
+                    <div class="wrapper d-flex align-items-start mt-3" id="information-bar">
+                        <ul>
+                            <li>
+                                <i class="im im-location"></i>
+                            </li>
+                            <li><span><strong>Location</strong><span class="location">Tobie's Room - A204</span></span></li>
+                        </ul>
+                    </div>
+                    <div class="wrapper d-flex align-items-start mt-3" id="information-bar">
+                        <ul>
+                            <li>
+                                <i class="im im-clock"></i>
+                            </li>
+                            <li><span><strong>Time</strong> <span class="time">02:30 PM</span></span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-function feedMainEvent() {
-  let data = upComingEvents[0];
-  let eventTitle = $(`[event-title="main"]`);
-  let eventDate = $(`[event-date="main"]`);
-  let eventTime = $(`[event-time="main"]`);
-  let eventLocation = $(`[event-location="main"]`);
-  let eventDesc = $(`[event-desc="main"]`);
+        </div>
+*/
 
-  eventTitle.text(data.title);
-  eventDate.text(data.date);
-  eventTime.text(data.time);
-  eventLocation.text(data.location);
-  eventDesc.text(data.description);
+function createNewEvent({ dateNumber, month, title, location, time }) {
+	const createInfo = (icon, placeHolder, classType, text) => {
+		let itemWrapper = $('<div>').addClass('wrapper d-flex align-items-start mt-3').attr('id', 'information-bar');
+		let itemList = $('<ul>');
+		let itemIcon = $('<li>').append($('<i>').addClass(`im im-${icon}`));
+		let item = $('<li>').append(
+			$('<span>').append($('<strong>').text(placeHolder)).append($('<span>').addClass(classType).text(text))
+		);
+
+		let fullItem = itemWrapper.append(itemList.append(itemIcon).append(item));
+
+		return fullItem;
+	};
+
+	// Main Parent
+	let mainParent = $('<div>').addClass('event-content justify-content-start text-left');
+
+	let mainWrapper = $('<div>').addClass('wrapper d-flex flex-row pl-3 pr-3');
+
+	let dateWrapper = $('<div>').addClass(
+		'wrapper date mr-5 text-center d-flex flex-column align-items-center justify-content-center'
+	);
+	let dateNumberEl = $('<h1>').addClass('date-number').attr('id', 'testing-dates').text(dateNumber);
+	let monthEl = $('<h4>').addClass('month').text(month);
+
+	let infoWrapper = $('<div>').addClass('wrapper info-bar time-location d-flex justify-content-between flex-column');
+
+	let titleInfo = createInfo('info', '', 'title', title);
+
+	let locationInfo = createInfo('location', 'Location', 'location', location);
+
+	let timeInfo = createInfo('clock', 'Time', 'time', time);
+
+	let hr = $('<hr>').addClass('my-4');
+
+	return mainParent
+		.append(hr)
+		.append(
+			mainWrapper
+				.append(dateWrapper.append(dateNumberEl).append(monthEl))
+				.append(infoWrapper.append(titleInfo).append(locationInfo).append(timeInfo))
+		);
 }
 
-function feedOverflowEvent() {
-	for (let i = 1; i < upComingEvents.length; i++) {
-		let eventJSON = upComingEvents[i];
-		let eventTitle = $(`[event-title="${i}"]`);
-		let eventDate = $(`[event-date="${i}"]`);
-		let eventTime = $(`[event-time="${i}"]`);
-		let eventLocation = $(`[event-location="${i}"]`);
-		let eventDesc = $(`[event-desc= "${i}"]`);
+// TODO Get events from Database and feed it to the browser
+const getAndFeedEvents = new Promise(() => {
+	let http = new XMLHttpRequest();
 
-		eventTitle.text(eventJSON.title);
-		eventDate.text(eventJSON.date);
-		eventTime.text(eventJSON.time);
-		eventLocation.text(eventJSON.location);
-		eventDesc.text(eventJSON.description);
-	}
-}
+	const url = `${window.location.href}events`;
 
+	http.open('GET', url, true);
 
-$(document).ready(() => {
-  feedMainEvent();
-
-  if (upComingEvents.length > 1) {
-    feedOverflowEvent();
-  } else {
-    $('.event-overflow').css('display', 'none');
-  }
-  
-  if (upComingEvents.length <= 4) $('.event-overflow-3').css('display', 'none');
-
-  if (upComingEvents.length <= 3) $('.event-overflow-2').css('display', 'none');
+	http.onreadystatechange = () => {
+		if (http.readyState == 4 && http.status == 200) {
+			JSON.parse(http.response).forEach(d => {
+				let eventData = {
+					title      : d.title,
+					dateNumber : d.date.split(' ')[1],
+					month      : d.date.split(' ')[0],
+					location   : d.location,
+					time       : d.time
+				};
+				$('div.container.events').append(createNewEvent(eventData));
+			});
+		}
+	};
+	http.send();
 });
 
+$(document).ready(async () => {
+	await getAndFeedEvents;
+
+	// If there is no event, append a text saying there is no upcoming event
+	setTimeout(() => {
+		if ($('.event-content').length < 1) {
+			let noEvent = $('<h3>')
+				.addClass('text-black-50 display-4 text-center')
+				.text('There is currently no upcoming events');
+			$('div.container.events').append(noEvent);
+		}
+	}, 1);
+});
