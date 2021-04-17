1d40c5b0f670882de995c5bd789b7212e4e7949a
Joshua Pelealu <hasianjoshua@gmail.com>
2021-04-16 21:47:03 -0400

    💡frontend ~ added intersection component for intersection observer

---
+++ src/lib/utils/Intersection.svelte
@@ -0,0 +1,18 @@
+<script>
+    import IntersectionObserver from 'svelte-intersection-observer';
+    import {createEventDispatcher} from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    let intersecting;
+    let element;
+    
+    // If the variable intersecting is true, then dispatch the 'intersecting event'
+    $: if (intersecting) dispatch('intersecting')
+    else dispatch('not-intersecting')
+    
+</script>
+
+<IntersectionObserver {element} bind:intersecting>
+    <div class="h-1 intersection" bind:this={element}></div>
+</IntersectionObserver>
