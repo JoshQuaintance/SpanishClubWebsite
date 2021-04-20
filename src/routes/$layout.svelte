72f151a1761b21780e804fea8dbafadcc9200dcc
Joshua Pelealu <hasianjoshua@gmail.com>
2021-04-20 07:54:44 -0400

    💡 frontend ~ change default layout to include header

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,5 +1,17 @@
 <script>
-	import '../app.postcss';
+    import '../app.postcss';
+    import Header from '$lib/Header/Header.svelte';
+    import Intersection from '$lib/utils/tools/Intersection.svelte';
+    import Spacing from '$lib/utils/tools/Spacing.svelte';
+
+	let scrolled = false;
 </script>
 
+<Header id={scrolled ? 'scrolled' : ''} />
+<Intersection
+    on:intersecting={() => (scrolled = false)}
+    on:not-intersecting={() => (scrolled = true)}
+/>
+
+<Spacing margin="2rem" />
 <slot />
