c55807ca391e09ab97f431977419de5a060685fe
Joshua Pelealu <hasianjoshua@gmail.com>
2021-04-20 07:55:06 -0400

    💡 frontend ~ remove header that is already in default layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,6 @@
 <script>
     // Page Components
-    import Header from '$lib/Header/Header.svelte';
     import Introduction from '$lib/Introduction/Introduction.svelte';
-
-    // Utility Components
-    import Intersection from '$lib/utils/tools/Intersection.svelte';
-    import Spacing from '$lib/utils/tools/Spacing.svelte';
-
-    let scrolled;
 </script>
 
 <svelte:head>
@@ -15,14 +8,6 @@
 
     <title>Home | Sidney Spanish Club</title>
 </svelte:head>
-
-<Header id={scrolled ? 'scrolled' : ''} />
-<Intersection
-    on:intersecting={() => (scrolled = false)}
-    on:not-intersecting={() => (scrolled = true)}
-/>
-
-<Spacing margin="2rem" />
 
 <Introduction />
 
