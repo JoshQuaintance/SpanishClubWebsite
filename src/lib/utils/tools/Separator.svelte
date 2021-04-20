00fb39ae0e6d5e1ceb177b9b13d1077021efa678
Joshua Pelealu <hasianjoshua@gmail.com>
2021-04-19 22:09:45 -0400

    💡 frontend ~ added a separator component

---
+++ src/lib/utils/tools/Separator.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let margin;
+
+    // 0 - Horizontal | 1 - Vertical
+    export let direction = 0;
+    export let length;
+    export let color;
+    export let thick;
+
+    let widthOrHeight = direction == 0 ? `min-width: ${length}` : `min-height: ${length}`;
+</script>
+
+<span
+    class="separator"
+    style="{widthOrHeight}; margin: {margin}; border-color: {color}; border-width: {thick};"
+/>
+
+<style>
+    .separator {
+        border: solid 3px black;
+        border-radius: 15px;
+    }
+</style>
