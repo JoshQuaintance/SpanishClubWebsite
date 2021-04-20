dbbcb712322b74f0f6b5fceb657a2654453cb3f0
Joshua Pelealu <hasianjoshua@gmail.com>
2021-04-20 07:54:31 -0400

    ✨ style ~ separator in animation

--- src/lib/utils/tools/Separator.svelte
+++ src/lib/utils/tools/Separator.svelte
@@ -7,17 +7,30 @@
     export let color;
     export let thick;
 
-    let widthOrHeight = direction == 0 ? `min-width: ${length}` : `min-height: ${length}`;
+    let widthOrHeight = direction == 0 ? `width: ${length} ` : `height: ${length};`;
 </script>
 
-<span
-    class="separator"
-    style="{widthOrHeight}; margin: {margin}; border-color: {color}; border-width: {thick};"
-/>
+<!-- {direction == 0 ? 'w-full' : 'h-full'} -->
+<div class="separator-container relative">
+    <span
+        class="separator inline-block"
+        style="{widthOrHeight}; margin: {margin}; border-color: {color}; border-width: {thick};"
+    />
+</div>
 
 <style>
+
+    @keyframes separator-anim {
+        from {
+            width: 0;
+            height: 0;
+        }
+    }
     .separator {
+        @apply text-center;
+
         border: solid 3px black;
         border-radius: 15px;
+        animation: separator-anim .7s ease-in-out forwards;
     }
 </style>
